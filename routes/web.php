08d6a999516c0173bc7ee7791b6e2323aa9b2ed5
faizanmirzabaig <mirzafaizan1931@gmail.com>
2022-09-07 06:08:54 +0530

    sdasdasdsad

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('admin/logout',[AdminController::class,'destroy'])->name('admin.logout');
 
 Route::get('/', function () {
     return view('welcome');
